1d67693139ca18011f644e8e62ce2587166d2952
mynkq <refreshedmayank@gmail.com>
2026-05-28 18:42:58 +0530

    register api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,10 @@
+const cookieparser = require('cookie-parser');
 const authRoute = require('./routes/auth.route');
 const express = require('express');
 const app = express();
-app.use(express());
+app.use(express.json())
+app.use(cookieparser())
 app.use("/api/auth", authRoute);
+
 
 module.exports = app;
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,34 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const authRoute = require("../routes/auth.route")
 const UserModel = require("../models/user.model");
 async function Register(req, res){
+    console.log("Request Recieved", req.body) // testing purpose
+    const username = req.body.username; 
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(username, email, password) // testing purpose
+
+    const isuserexists = await UserModel.findOne({
+        $or:[{Username: username},{Email: email}]
+    })
+
+    if(isuserexists){return res.status(401).json({Message: "User already exist."})}
+    const hashedpassword = await bcrypt.hash(password, 10);
+    const registeruser = await UserModel.create({
+        Username: username,
+        Email: email,
+        Password: hashedpassword,
+
+     })
+
     
+        const token = jwt.sign({id: registeruser._id,}, process.env.Jwt_Secret);
+    
+    res.cookie("token", token);
+    res.status(201).json({Message: "User registerd!"})
+
+
 
 }
 module.exports = {Register};
