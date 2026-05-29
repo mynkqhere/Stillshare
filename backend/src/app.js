2e5fd79336927b4097820984da864bbda8243b33
mynkq <refreshedmayank@gmail.com>
2026-05-29 13:59:09 +0530

    docs: listed created apis

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const cookieparser = require('cookie-parser');
+const postRoute = require("./routes/post.routes");
 const authRoute = require('./routes/auth.route');
 const express = require('express');
 const app = express();
 app.use(express.json())
 app.use(cookieparser())
 app.use("/api/auth", authRoute);
+app.use("/api/post/", postRoute);
 
 
 module.exports = app;
---
+++ docs/api.md
@@ -0,0 +1,4 @@
+Our Auth Api
+/api/auth/register
+/api/auth/login
+/api/auth/logout
