243e1e094407820e8bc4509b54ae2555a4d56d9d
mynkq <refreshedmayank@gmail.com>
2026-05-30 17:52:04 +0530

    backend - cors added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
+const cors = require('cors');
 const cookieparser = require('cookie-parser');
 const postRoute = require("./routes/post.routes");
 const authRoute = require('./routes/auth.route');
 const express = require('express');
 const app = express();
-app.use(express.json())
-app.use(cookieparser())
+app.use(cors());
+app.use(express.json());
+app.use(cookieparser());
 app.use("/api/auth", authRoute);
 app.use("/api/post/", postRoute);
 
