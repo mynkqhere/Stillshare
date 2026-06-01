d1df1e5254ffe0cb42af579aac890295a154f4f9
mynkq <refreshedmayank@gmail.com>
2026-06-01 16:16:21 +0530

    displayed errors and sucess messages to user

--- frontend/src/app/Components/register.tsx
+++ frontend/src/app/Components/register.tsx
@@ -1,7 +1,10 @@
 "use client";
 import axios from "axios";
 import { error } from "console";
+import { useState } from "react";
 function Register(){
+    const [errormessage, setErrorMessage] = useState("");
+    const [successmessage, setSucessMessage] = useState("");
 async function handlesubmit(e: any){
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -14,10 +17,13 @@ async function handlesubmit(e: any){
 
      })
     console.log("Registration Successfull")
+    setSucessMessage(response.data.Message);
     console.log(response.data.Message);
     }catch(error){
         console.error("Error during registration:"),
-        console.error(error.response.data.Message)}
+        console.error(error.response.data.Message);
+        setErrorMessage(error.response.data.Message)
+    };
      }
         return (<div className=" bg-black min-h-screen border flex flex-col items-center justify-center p-4 " >
         <span className="text-white font-bold ">Register</span>
@@ -26,6 +32,9 @@ async function handlesubmit(e: any){
             <input className=" text-white p-2 border rounded-sm " type="text" name="email" placeholder="Email" />
             <input className=" text-white p-2 border rounded-sm " type="text" name="password" placeholder="Password" />
             <button className="text-white p-2 border rounded-sm" type="submit" >Submit</button>
+        {errormessage && <span className="text-red-500" >{errormessage}</span>}
+        {successmessage && <span className="text-green-500" >{successmessage}</span>}
+        
         </form>
     </div>)
 }
