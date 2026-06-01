9af2cb9d5585e781b9cef178200aaa0a0f6fb41a
mynkq <refreshedmayank@gmail.com>
2026-06-01 18:28:06 +0530

    ..

--- frontend/src/app/Components/register.tsx
+++ frontend/src/app/Components/register.tsx
@@ -1,6 +1,5 @@
 "use client";
 import axios from "axios";
-import { error } from "console";
 import { useState } from "react";
 function Register(){
     const [errormessage, setErrorMessage] = useState("");
@@ -10,7 +9,7 @@ async function handlesubmit(e: any){
     const formData = new FormData(e.target);
     console.log(formData) // testing if formdata is being captured correctly.
     try{
-    const response = await axios.post("https://stillshare-y7ku.onrender.com/api/auth/register", {
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
         username: formData.get("username"),
         email: formData.get("email"),
         password: formData.get("password"),
@@ -20,7 +19,7 @@ async function handlesubmit(e: any){
     setSucessMessage(response.data.Message);
     
     console.log(response.data.Message);
-    }catch(error){
+    }catch(error: any){
         console.error("Error during registration:"),
         console.error(error.response.data.Message);
         setErrorMessage(error.response.data.Message)
@@ -32,7 +31,7 @@ async function handlesubmit(e: any){
         <form onSubmit={handlesubmit} className="flex flex-col items-center  p-4 gap-4 ">
             <input onChange={()=>{setSucessMessage(""); setErrorMessage("");}} className=" text-white p-2 border rounded-sm " type="text" name="username" placeholder="Username" />
             <input onChange={()=>{setSucessMessage(""); setErrorMessage("");}} className=" text-white p-2 border rounded-sm " type="text" name="email" placeholder="Email" />
-            <input onChange={()=>{setSucessMessage(""); setErrorMessage("");}} className=" text-white p-2 border rounded-sm " type="text" name="password" placeholder="Password" />
+            <input className=" text-white p-2 border rounded-sm " type="text" name="password" placeholder="Password" />
             <button className="text-white p-2 border rounded-sm" type="submit" >Submit</button>
         {errormessage && <span className="text-red-500 text-sm" >{errormessage}</span>}
         {successmessage && <span className="text-green-500 text-sm " >{successmessage}</span>}
