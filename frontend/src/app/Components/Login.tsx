0b7d282e6b7d013afb594185dcd1cd55a183532a
mynkq <refreshedmayank@gmail.com>
2026-06-03 13:03:02 +0530

    done logout

--- frontend/src/app/Components/Login.tsx
+++ frontend/src/app/Components/Login.tsx
@@ -1,7 +1,36 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 function Loginform(){
-    return(<div className=" bg-black min-h-screen border flex flex-col items-center justify-center p-4 " >
+const router = useRouter();
+    async function handlesubmit(e: any){
+    e.preventDefault();
+    const loginformdata = new FormData(e.target)
+    console.log(loginformdata)
+
+    try{
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`,{
+            username: loginformdata.get("username"),
+            email: loginformdata.get("email"),
+            password: loginformdata.get("password")
+        },{withCredentials: true})
+        console.log("login successfull")
+        console.log(response)
+        router.push("/Feed")
+        
+    }
+    catch(error: any){
+        console.error("login failed")
+        console.error(error.response)
+    }
+
+
+}
+
+
+  return(<div className=" bg-black min-h-screen border flex flex-col items-center justify-center p-4 " >
         <span className="text-white font-bold ">Login</span>
-        <form className="flex flex-col items-center  p-4 gap-4 ">
+        <form onSubmit={handlesubmit} className="flex flex-col items-center  p-4 gap-4 ">
             <input className=" text-white p-2 border rounded-sm " type="text" name="username" placeholder="Username" />
             <input className=" text-white p-2 border rounded-sm " type="text" name="email" placeholder="Email" />
             <input className=" text-white p-2 border rounded-sm " type="text" name="password" placeholder="Password" />
