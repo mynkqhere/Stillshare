f9646fc94b724e3e1f7c21d9d3864f290b0df198
refreshedmayank <refreshedmayank@gmail.com>
2026-06-12 13:07:11 +0530

    defined how post card will look add: username and caption basic styled for prototyping

--- frontend/src/app/Components/postcard.tsx
+++ frontend/src/app/Components/postcard.tsx
@@ -1,6 +1,12 @@
+import styles from "../css/signup.module.css"
 function PostCard(props: any){
-    return(<div>
-        <img src={props.post} alt="" />
+    return(
+    <div className={styles.Feed}>
+        <div className={styles.postcard} >
+        <span>{ props.username}</span>
+        <img src={props.post} alt={props.caption} />
+        <span>{props.caption}</span></div>
+        
     </div>)
 }
 export default PostCard;
