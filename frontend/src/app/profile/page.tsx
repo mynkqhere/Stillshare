a8a5e0252f84cb3318891f7b0850a9a0b63059e5
refreshedmayank <refreshedmayank@gmail.com>
2026-06-15 13:20:33 +0530

    added footer

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "../Components/footer";
 import ProfileOperation from "../Components/Profileops";
 
 function Profile(){
     return(<div>
         This is Profile Location
         <ProfileOperation/>
+        <Footer/>
         </div>)
 }
 export default Profile;
