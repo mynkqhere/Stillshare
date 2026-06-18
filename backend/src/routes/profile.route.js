fb78d2f12079acfbe900e013ae808bd9723cbcc9
refreshedmayank <refreshedmayank@gmail.com>
2026-06-18 15:58:29 +0530

    add: defined route to update of change bio

--- backend/src/routes/profile.route.js
+++ backend/src/routes/profile.route.js
@@ -7,4 +7,6 @@ const upload = multer({storage: multer.memoryStorage()});
 router.post('/create-profile',Auth.Auth, upload.single("Profilepicture"),profileController.CreateProfile);
 router.get('/get-user/:id', profileController.GetProfile)
 router.post("/change-profilepicture/:id", Auth.Auth, upload.single("Profilepicture") ,profileController.Changeprofilepicture)
+router.post('/change-name/:id', Auth.Auth, profileController.Changename)
+router.post('/change-bio/:id', Auth.Auth, profileController.Changebio)
 module.exports = router; 
