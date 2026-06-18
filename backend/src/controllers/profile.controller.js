99cfead7dc1b22c6b8f08bbd7c274a3e0e795250
refreshedmayank <refreshedmayank@gmail.com>
2026-06-18 15:36:02 +0530

    add: added functionality to update profile picture

--- backend/src/controllers/profile.controller.js
+++ backend/src/controllers/profile.controller.js
@@ -1,6 +1,7 @@
 const Upload = require('../services/storage.service');
 const ProfileModel = require('../models/profile.model');
 async function CreateProfile(req, res){
+    console.log(req.body)
     const Buffer = req.file.buffer;
     const fileName = req.file.originalname
     const result = await Upload(Buffer, fileName) 
@@ -10,7 +11,10 @@ async function CreateProfile(req, res){
      Name: req.body.Name,
      User: req.user
 })
+
+if(!profile){return res.status(401).json({Message: "Invalid data"})}
 res.status(201).json({Message: "Profile Created successfully", profile})
+
 }
 async function GetProfile(req, res){
 const ID = req.params.id
@@ -18,6 +22,15 @@ const user = await ProfileModel.findOne({User: ID}).populate("User")
 if(!user){return res.status(400).json({Message: "User not found"})}
 res.status(201).json({Message: "User Fetched Successfully", user})
 }
+async function Changeprofilepicture(req, res){
+const ID = req.params.id
+const Buffer = req.file.buffer;
+const fileName = req.file.originalname
+const result = await Upload(Buffer, fileName)
+const updatedprofilepicture = await ProfileModel.findByIdAndUpdate(ID,{
+    Profilepicture: result.url
+})
+res.status(201).json({Message: "Profile Picture Updated Successfully"})
+}
 
-
-module.exports = {CreateProfile, GetProfile,};
+module.exports = {CreateProfile, GetProfile, Changeprofilepicture}
