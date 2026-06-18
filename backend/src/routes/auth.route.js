9149fc98014013e8d88deb9fd6d788bedd611c4c
refreshedmayank <refreshedmayank@gmail.com>
2026-06-18 14:43:31 +0530

    defined route to change username

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,9 @@
+const authMiddleware = require('../Middlewares/auth.post.middlewares')
 const authController = require("../controllers/auth.controller");
 const express = require('express');
 const router = express.Router();
 router.post('/register', authController.Register);
 router.post('/login', authController.Login);
 router.post("/logout", authController.Logout);
+router.post("/change-username/:id",authMiddleware.Auth, authController.Changeusername)
 module.exports = router;
