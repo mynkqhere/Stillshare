46131ae6998c7885efea1e3c56cf2654646955ad
refreshedmayank <refreshedmayank@gmail.com>
2026-06-19 16:20:04 +0530

    created: edit profile

---
+++ frontend/src/app/edit-profile/page.tsx
@@ -0,0 +1,8 @@
+import Editprofileops from "../Components/editprofileops";
+
+function Editprofile(){
+    return(<div>Edit profile page
+        <Editprofileops/>
+    </div>)
+}
+export default Editprofile;
