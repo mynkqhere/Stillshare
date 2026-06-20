a6efc29012c2af1bfeaa3078b562dbcf8014eddb
refreshedmayank <refreshedmayank@gmail.com>
2026-06-20 15:52:26 +0530

    basic profile card

--- frontend/src/app/Components/Profilecard.tsx
+++ frontend/src/app/Components/Profilecard.tsx
@@ -1,10 +1,17 @@
 // here i am going to write the component of profilecard 
+import { useRouter } from "next/navigation";
+import style from "../css/Profile.module.css"
 function ProfileCard(props: any){
-    return(<div>
-        <span>{props.username}</span>
-        <img src={props.image} alt={props.image} />
-        <span>{props.name}</span>
-        <span>{props.bio}</span>
+    const router = useRouter()
+    return(<div className={style.container} >
+        <div className={style.items}>
+        <span className={style.text} >{props.username}</span>
+        <div className={style.pfp}><img className={style.image} src={props.image} alt={props.image} /></div>
+        <span className={style.text} >{props.name}</span>
+        <span className={style.text} >{props.bio}</span>
+        <button onClick={()=> router.push("/edit-profile")}>Edit Profile</button>
+        <button>Message</button>
+         </div>
     </div>)
 }
 export default ProfileCard;
