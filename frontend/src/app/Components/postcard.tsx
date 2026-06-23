99294aefe294841d805ef7a6a2974183908c066d
refreshedmayank <refreshedmayank@gmail.com>
2026-06-23 12:55:55 +0530

    add: delete post

--- frontend/src/app/Components/postcard.tsx
+++ frontend/src/app/Components/postcard.tsx
@@ -1,11 +1,21 @@
+import axios from "axios";
 import styles from "../css/signup.module.css"
 function PostCard(props: any){
+    async function handledelete(postid: any){
+        try{
+            const posttobedeleted = postid
+            const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/post/delete-post/${posttobedeleted}`)
+            console.log("Successfully Deleted the clicked post", response)
+        }catch(error){console.error("Failed to delete this post", error)}
+    }
     return(
     <div className={styles.Feed}>
         <div className={styles.postcard} >
         <span>{ props.username}</span>
         <img src={props.post} alt={props.caption} />
         <span>{props.caption}</span></div>
+        <button onClick={()=> handledelete(props.postid)}>Delete</button>
+        
         
     </div>)
 }
