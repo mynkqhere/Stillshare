111518f6711d59353dae1c1186367bcb4097ca74
refreshedmayank <refreshedmayank@gmail.com>
2026-06-23 17:44:49 +0530

    add: search card basic

--- frontend/src/app/Components/searchcard.tsx
+++ frontend/src/app/Components/searchcard.tsx
@@ -1,6 +1,22 @@
+import { useRouter } from "next/navigation"
+import axios from "axios"
 import style from "../css/Search.module.css"
 function SearchCard(props: any){
-    return(<div className={style.Profilecontainer}>
+    const router = useRouter()
+    async function handleprofileclick(userid:any){
+        try{
+        const ID = userid
+        console.log(ID)
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/profile/get-user/${ID}`)
+        console.log("Successfully fetched user profile", response)
+      router.push('/userprofile')
+        
+
+    }catch(error){console.log("Failed to fetch user profile", error)}
+ 
+
+    }
+    return(<div onClick={()=> handleprofileclick(props.userid)} className={style.Profilecontainer}>
        <div> <img className={style.Profileimage} src={props.pfp} alt="" /></div>
         <span>{props.username}</span>
         <span>{props.name}</span>
