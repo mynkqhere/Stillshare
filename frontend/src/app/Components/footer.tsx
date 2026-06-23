5391dc5c2574848f39e9365e18de185d13168ac4
refreshedmayank <refreshedmayank@gmail.com>
2026-06-23 15:22:44 +0530

    add: directing to search page

--- frontend/src/app/Components/footer.tsx
+++ frontend/src/app/Components/footer.tsx
@@ -2,7 +2,7 @@ import styles from "../css/signup.module.css";
 function Footer(){
     return(<footer className={styles.footer}>
              <a className={styles.flink} href="/Home" >Home</a>
-             <span>Search</span>
+             <a href="/search">Search</a>
              <span>Message</span>
              <a className={styles.flink} href="/profile">Profile</a></footer>)
 }
