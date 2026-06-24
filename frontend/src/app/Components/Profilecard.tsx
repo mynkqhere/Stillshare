b0458623c3df53672e31f6ceeb290bd8f297594f
refreshedmayank <refreshedmayank@gmail.com>
2026-06-24 13:12:16 +0530

    fix: remove harcoded actions passed as props instead

--- frontend/src/app/Components/Profilecard.tsx
+++ frontend/src/app/Components/Profilecard.tsx
@@ -6,11 +6,11 @@ function ProfileCard(props: any){
     return(<div className={style.container} >
         <div className={style.items}>
         <span className={style.text} >{props.username}</span>
-        <span onClick={()=> router.push("/create")}>+</span>
+        <span onClick={props.onbutton2}>{props.button1}</span>
         <div className={style.pfp}><img className={style.image} src={props.image} alt={props.image} /></div>
         <span className={style.text} >{props.name}</span>
         <span className={style.text} >{props.bio}</span>
-        <button onClick={()=> router.push("/edit-profile")}>Edit Profile</button>
+        <button onClick={props.onbutton2click}>{props.button2}</button>
         <button>Message</button>
          </div>
     </div>)
